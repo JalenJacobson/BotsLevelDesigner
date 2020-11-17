d64685d188c109caf5fb3b61d82ab9e91db9ca57
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2020-11-17 09:11:00 -0700

    Lifter & Animation

---
+++ Assets/Scripts/General/Lifter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifter : MonoBehaviour
+{
+   public Animator anim;
+
+ // Use this for initialization
+ void Start () {
+        anim = GetComponent<Animator>();
+ }
+ 
+ // Update is called once per frame
+ void Update () {
+        if (Input.GetKeyDown("g"))
+        {
+            anim.Play("Lifter");
+
+        }
+    }
+}
