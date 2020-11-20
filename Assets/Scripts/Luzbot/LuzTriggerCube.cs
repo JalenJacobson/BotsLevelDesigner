c549cc445b26a45ac1fd7b86e892e19a0a007904
jacobsonj <justinjacobson0118@gmail.com>
2020-11-20 05:04:21 -0700

    added trigger to front of Luz

---
+++ Assets/Scripts/Luzbot/LuzTriggerCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzTriggerCube : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+     {
+         print(other.name.Contains("Sat"));
+         if(other.name.Contains("Sat")){
+             print("it worked");
+         print(other.name);
+         }
+         
+     }
+     void OnTriggerExit(Collider other)
+     {
+       
+     }
+}
