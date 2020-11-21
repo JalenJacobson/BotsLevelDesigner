d37087b1c80ab5a74db4bcbeb2cf049e3ce5c29c
jacobsonj <justinjacobson0118@gmail.com>
2020-11-20 19:43:07 -0700

    started brute lifting functionality

---
+++ Assets/Scripts/Brutebot/BruteTriggerCube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteTriggerCube : MonoBehaviour
+{
+    public bool triggerEntered = false;
+    public GameObject touching = null;
+    // public GameObject Brute;
+    public Vector3 liftPos;
+
+    void Start()
+    {
+        liftPos = new Vector3(0.0f, 1.0f, 0.0f);
+        print(GameObject.FindGameObjectWithTag("Brute").transform.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+     {
+         
+     }
+
+    void OnTriggerStay(Collider other)
+    {
+        // if(other.name.Contains("box")){
+             touching = other.gameObject;
+            //  Brute = Collider.gameObject;
+        // }
+    }
+
+     void OnTriggerExit(Collider other)
+     {
+         
+            touching = null;
+        
+       
+     }
+
+     void Update()
+     {
+         if(touching != null && Input.GetKeyDown("z"))
+         {
+             // need state "lifting" and function if(lifting = true){do the following thing}
+            touching.transform.position = GameObject.FindGameObjectWithTag("Brute").transform.position + liftPos;
+         }
+     }
+}
