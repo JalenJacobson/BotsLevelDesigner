23c96848a02fe7098e0ebfa8ec8ea6c9d6dfc538
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2020-11-24 11:17:20 -0700

    UI

---
+++ Assets/Textures/UI/BubbleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+   public GameObject ActionBubbles;
+   public Animator anim;
+    public bool triggerEntered = false;
+    public GameObject touching = null;
+
+
+ // Use this for initialization
+ void Start () {
+        anim = GetComponent<Animator>();
+ }
+ 
+ // Update is called once per frame
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name.Contains("Gear")){
+             touching = other.gameObject;
+        }
+    }
+     void Update()
+     {
+         if(touching != null)
+         {
+        anim.Play("ActionBubbleAnim");
+         }
+    }
+}
