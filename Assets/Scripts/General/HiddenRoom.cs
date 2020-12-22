c502dbf04492fd9a7f45d7fd41bf267574169008
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2020-12-21 18:59:09 -0700

    Hiding Rooms

---
+++ Assets/Scripts/General/HiddenRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ GetComponent<MeshRenderer>().enabled = true;       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    var characterName = other.name;
+    if(characterName == "Brute" || characterName == "IdleLuz" || characterName == "Gears" || characterName == "SatBot" || characterName == "Pump"){
+    
+ GetComponent<MeshRenderer>().enabled = false;
+    }
+    }
+        void OnTriggerExit(Collider other)
+    {
+GetComponent<MeshRenderer>().enabled = true;        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
