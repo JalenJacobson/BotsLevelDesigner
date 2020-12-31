00d61110c404e6edcac5a06faa3d8dc4bbee27e1
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2020-12-31 15:00:28 -0700

    Luz Room

---
+++ Assets/Scripts/General/HiddenRoomLuz.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoomLuz : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ GetComponent<MeshRenderer>().enabled = true;       
+    }
+    void OnTriggerStay(Collider other)
+    {
+    var characterName = other.name;
+    if(characterName == "IdleLuz"){
+    
+ GetComponent<MeshRenderer>().enabled = false;
+    }
+    }
+        void OnTriggerExit(Collider other)
+    {
+GetComponent<MeshRenderer>().enabled = true;        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
