9abcb9a5313323456cdd1c827e0cc02fad9779ae
jacobsonj <justinjacobson0118@gmail.com>
2021-01-05 05:34:35 -0700

    changing asset folder scructure

---
+++ Assets/Scripts/Items/Luz/Lifter/ReadMe_LuzLifter.cs
@@ -0,0 +1,9 @@
+﻿// To give luzlifter functionality
+// 1. include luzlifter object into scene.
+// 2. include powerconnection box into scene.
+// 3. create a new c# script titled Lifter + level + phase + item number with underscore in between.
+//  for example if I were adding a lifter to level 1 phase 3 and it was the second lifter, 
+//  my script name would be Lifter_01_03_2
+//  3a. 
+// 4. add script to lifter item
+// 5. create enw c# script titled Powerconnection_Lifter (and then the same level phase and item numbers)
