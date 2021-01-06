00808ab195b8b6625b5dc0fa362a7ad3231f10a5
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2021-01-06 00:13:44 -0700

    fayse thryie

---
+++ Assets/Scripts/General/FallWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallWall : MonoBehaviour
+{
+public GameObject Wall;
+   public Animator anim;
+
+       void Start()
+       {
+        anim = GetComponent<Animator>();
+       }
+     void OnTriggerEnter(Collider other)
+     {
+             var characterName = other.name;
+    if(characterName == "Brute" || characterName == "IdleLuz" || characterName == "Gears" || characterName == "SatBot" || characterName == "Pump")
+    {
+             anim.Play("FallWall"); 
+         }
+     }
+              
+}
+ 
