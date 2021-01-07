a399fd3cc617cc864f4164dab881c9c218a1382b
jacobsonj <justinjacobson0118@gmail.com>
2021-01-06 19:14:14 -0700

    started logic flow between levels

---
+++ Assets/Level_Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Manager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
