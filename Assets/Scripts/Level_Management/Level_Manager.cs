8ed8dd43ca18d7c81a9805f61a7a75668cf7c635
jacobsonj <justinjacobson0118@gmail.com>
2021-01-07 05:33:02 -0700

    Scene to Scene flow

---
+++ Assets/Scripts/Level_Management/Level_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Manager : MonoBehaviour
+{
+    public int sceneToGoTo;
+    void Start()
+    {
+        sceneToGoTo = 1;
+    }
+
+    void Update()
+    {
+        if(sceneToGoTo != 1 && Input.GetKeyDown("q"))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+
+        SceneManager.LoadScene(sceneToGoTo);
+        // sceneToGoTo = 1;
+    }
+
+    public void setSceneToGoTo(int nextScene)
+    {
+        sceneToGoTo = nextScene;
+    }
+}
---
+++ Assets/Scripts/Level_Management/Level_Selector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_Selector : MonoBehaviour
+{
+    public GameObject Level_Manager;
+    Level_Manager LevelManager_script;
+    public int newScene;
+
+    void Start()
+    {
+        LevelManager_script = Level_Manager.GetComponent<Level_Manager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+        LevelManager_script.setSceneToGoTo(newScene);
+        // other.transform.position = levelBeaconPosition;
+        // other.transform.position = levelBeaconPosition;
+
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        LevelManager_script.setSceneToGoTo(1);
+    }
+}
