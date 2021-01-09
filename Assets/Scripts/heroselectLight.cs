64224593d4663e95a3b2c1b16b5f2d8e939c7385
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2021-01-09 11:15:00 -0700

    Transition

---
+++ Assets/Scripts/heroselectLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroselectLight : MonoBehaviour
+{
+    public GameObject Lights;
+   public Animator anim;
+    // public bool displayBubble = false;
+
+
+ // Use this for initialization
+ void Start () {
+        anim = GetComponent<Animator>();
+ }
+ 
+ // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+
+   public void lights()
+   {
+       anim.Play("HeroSelectLights");
+   }
+   
+}
