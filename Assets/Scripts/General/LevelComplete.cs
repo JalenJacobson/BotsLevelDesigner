d5543c4f310951d8a1974c8dea883c6cdee29e7c
jacobsonj <justinjacobson0118@gmail.com>
2021-01-12 07:56:24 -0700

    Endgame with virus sequence

--- Assets/Scripts/General/LevelComplete.cs
+++ Assets/Scripts/General/LevelComplete.cs
@@ -1,21 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelComplete : MonoBehaviour
 {
 
-    public int playersReady = 0;
+    public int playersReady;
+    public float gameTimeRemaining;
+    public bool virusDelivered = false;
+    public Text consoleTimeRemaining;
+    public Color redDanger;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        redDanger = new Color(1f, 0.1f, 0.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        consoleTimeRemaining.text = gameTimeRemaining.ToString("0.00");
+        if(virusDelivered == true)
+        {
+            if(gameTimeRemaining > 0)
+            {
+                gameTimeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                gameTimeRemaining = 0f;
+                print("Game Over");
+            }
+            if(playersReady == 5)
+            {
+                gameTimeRemaining = 0f;
+                print("Level complete");
+            }
+        }
+        else
+        {
+            if(playersReady < 5)
+            {
+                gameTimeRemaining -= Time.deltaTime;
+            }
+        }
     }
 
     public void addPlayerReady()
@@ -25,5 +55,12 @@ public class LevelComplete : MonoBehaviour
     public void removePlayerReady()
     {
         playersReady -= 1;
+    }
+
+    public void deliverVirus()
+    {
+        gameTimeRemaining = 30f;
+        consoleTimeRemaining.color = redDanger;
+        virusDelivered = true;
     }
 }
--- Assets/Scripts/Satbot/Sat_Upload_1.cs
+++ Assets/Scripts/Satbot/Sat_Upload_1.cs
@@ -7,12 +7,16 @@ public class Sat_Upload_1 : MonoBehaviour
     public string token = "1";
     public GameObject forcegate_gate;
     ForceGate forcegate_script;
+    
+    public GameObject StartPort;
+    public LevelComplete levelcomplete_script;
 
     
     // Start is called before the first frame update
     void Start()
     {
         forcegate_script = forcegate_gate.GetComponent<ForceGate>();
+        levelcomplete_script = StartPort.GetComponent<LevelComplete>();
     }
 
     // Update is called once per frame
@@ -30,6 +34,7 @@ public class Sat_Upload_1 : MonoBehaviour
         else if(uploadType == "endGame")
         {
             print("activate endgame sequence");
+            levelcomplete_script.deliverVirus();
         }
         
     }
