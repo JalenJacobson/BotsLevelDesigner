f3936f9efd435da8952e9c47899dbdd42ac94680
jacobsonj <justinjacobson0118@gmail.com>
2021-01-20 06:31:30 -0700

    Added map functionality

---
+++ Assets/Scripts/General/MiniMap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public GameObject rawImage;
+    public bool isActive;
+    
+    void Start()
+    {
+        isActive = false;  
+        rawImage.SetActive(isActive); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // renderActive();
+        if (Input.GetKeyDown("m"))
+        {
+            toggleActive();
+        }
+    }
+
+    public void toggleActive()
+    {
+        isActive = !isActive;
+        rawImage.SetActive(isActive);
+    }
+
+    // public void renderActive()
+    // {
+    //     if(isActive)
+    //     {
+    //         rawImage.SetActive(true);
+    //     }
+    //     else{rawImage.SetActive(false);}
+    // }
+}
