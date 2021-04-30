eae29497f9e0c1e26887a180bbb108460eb3ddd3
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2021-04-30 08:33:07 -0600

    Mini Map Icons

---
+++ Assets/Scripts/General/MiniMapGreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapGreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ GetComponent<MeshRenderer>().enabled = false;       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    var characterName = other.name;
+    if(characterName == "Brute" || characterName == "IdleLuz" || characterName == "Gears" || characterName == "SatBot" || characterName == "Pump"){
+    
+ GetComponent<MeshRenderer>().enabled = true;
+    }
+    }
+
+}
---
+++ Assets/Scripts/General/MiniMapRed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapRed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ GetComponent<MeshRenderer>().enabled = true;       
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    var characterName = other.name;
+    if(characterName == "Brute" || characterName == "IdleLuz" || characterName == "Gears" || characterName == "SatBot" || characterName == "Pump"){
+    
+ GetComponent<MeshRenderer>().enabled = false;
+    }
+    }
+
+}
